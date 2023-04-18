edf28612308d3e63fad2d0d15fad3d216b7feb14
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-04-19 00:39:22 +0200

    server http added again

--- server.mjs
+++ server.mjs
@@ -1,6 +1,15 @@
 import staticHandler from './staticHandler.mjs'
 import { readFileSync } from 'fs';
 import { createSecureServer } from 'http2';
+import { createServer } from 'http';
+
+const server = createServer((req, res) => {
+  staticHandler(req, res)
+})
+
+server.listen(4000, () => {
+  console.log('Server running at http://localhost:4000/')
+})
 
 const http2Server = createSecureServer({
   key: readFileSync('key.pem'),
@@ -9,6 +18,6 @@ const http2Server = createSecureServer({
   staticHandler(req, res);
 });
 
-http2Server.listen(4000, () => {
-  console.log('HTTP/2 server running at https://localhost:4000/');
+http2Server.listen(4001, () => {
+  console.log('HTTP/2 server running at https://localhost:4001/');
 });
